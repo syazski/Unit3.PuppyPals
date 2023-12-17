9ffbbcfa7fb4e54d18482f78ba38f482e5a292a5
Syaz S <syazski@gmail.com>
2023-12-16 23:10:45 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  //console.log(puppies);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+  
   return (
-    <>
-      <div>
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+              return (<p onClick={() => { setFeatPupId(puppy.id)}}
+              key={puppy.id}>{puppy.name}</p>
+            )
+          })
+          }
+          <section>
+          {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+          </section>  
+
         </div>
-    </>
+   
   )
 }
 
